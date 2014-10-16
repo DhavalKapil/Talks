3c4dbc30e3e71d015c6b32705e4186a3097a9df1
Abhijeet Kaur <abhijeetkaur96@gmail.com>
2014-10-17 01:29:31 +0530

    Adding the final Message class

--- src/talks/models/Message.java
+++ src/talks/models/Message.java
@@ -1,6 +1,19 @@
-package talks.models;
+package models
+
+import java.util.Date;
+
 
 public class Message
 {
+	private int statusCode;
+	private int creatorId;
+	private String message;
+	private long timestamp;
+
+	public void addTimestamp()
+	{
+		Date current = new Date();
+		timestamp = current.getTime();
+	}
 	
 }
