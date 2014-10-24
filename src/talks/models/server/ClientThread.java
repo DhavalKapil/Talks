fee91d8f4e36cfba15c500ab92aacf5239e1de2e
Abhijeet Kaur <abhijeetkaur96@gmail.com>
2014-10-25 01:33:47 +0530

    implemented the join chat room function

--- src/talks/models/server/ClientThread.java
+++ src/talks/models/server/ClientThread.java
@@ -23,6 +23,33 @@ class ClientThread extends Thread
 		node.sendMessage(message);
 	}
 
+	/**
+	 * @param: message , node
+	 * @return: message: indicating whether it has joined a chatroom or not
+	 *           001 -> chatroom joined
+	 *           002 -> chatroom not found or wrong password 
+	 */
+	public Message joinChatRoom(String message, Node node)
+	{
+		Message returnMessage;
+		String[] identifierAndPassword=message.split("\n");
+
+		for(int i=0; i< chatRoomList.size();i++)
+		{
+			if(chatRoomList.get(i).identifier==identifierAndPassword[0])
+			{
+				if(chatRoomList.get(i).password==identifierAndPassword[1])
+				{
+					insertNode(node);
+					returnMessage.setStatusCode(001);
+					return returnMessage;
+				}
+			}
+		}
+		returnMessage.setStatusCode(002);
+		return returnMessage;
+	}
+
 	public void run()
 	{
 		try
@@ -37,6 +64,7 @@ class ClientThread extends Thread
 				switch(message.getStatusCode())
 				{
 					// The protocol listing goes here
+					case 102 : joinChatRoom(message,node);
 				}
 			}
 		}
